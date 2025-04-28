2d1b0477295d71e1b349d5f744c25c5e8240d639
tobe <583590744@qq.com>
2025-04-28 22:09:43 +0800

    增加github page访问前缀

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  // NOTE: github 会额外增加path前缀，需要构建额外一个 baseurl
+  // TODO: 注入后不能支持 dist 预览了
+  const base = env.VITE_BASE_URL || '/'
+  const port = env.VITE_PORT || 5178
+  return {
+    base,
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
+    server: {
+      port: 5178,
+      host: true,
+    },
+  }
 })
